be264778d2e361f4a64d27ea691d47498f676141
lludko2 <lludko2@uic.edu>
2017-03-05 03:59:47 -0600

    movement addition

--- Assets/Scripts/PlayerMovementMouse.cs
+++ Assets/Scripts/PlayerMovementMouse.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovementMouse : MonoBehaviour {
 
     public float rotationSpeed;
     public float rotX;
@@ -12,16 +12,19 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         rotX += Input.GetAxis("Mouse X") * rotationSpeed;
         rotY += -Input.GetAxis("Mouse Y") * rotationSpeed;
         if (rotY > maxLookUpAngle) rotY = maxLookUpAngle;
         if (rotY < maxLookDownAngle) rotY = maxLookDownAngle;
 
         gameObject.transform.rotation = Quaternion.Euler(rotY, rotX, 0.0f);
+        //gameObject.transform.parent.transform.rotation = Quaternion.Euler(0, rotX, 0.0f);
+            
 	}
 }
---
+++ Assets/Scripts/PlayerMovementTraversal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTraversal : MonoBehaviour {
+
+    public float accel;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+    void Update() {
+        Vector3 childForward = transform.GetChild(0).gameObject.transform.forward;
+        Vector3 childRight = transform.GetChild(0).gameObject.transform.right;
+        childForward.y = 0; childRight.y = 0;
+        if(rb.velocity.magnitude < 5.0f && Input.GetKey(KeyCode.W))
+            rb.AddForce(childForward * accel);
+        if (rb.velocity.magnitude < 5.0f && Input.GetKey(KeyCode.S))
+            rb.AddForce(-childForward * accel);
+        if (rb.velocity.magnitude < 5.0f && Input.GetKey(KeyCode.A))
+            rb.AddForce(-childRight * accel);
+        if (rb.velocity.magnitude < 5.0f && Input.GetKey(KeyCode.D))
+            rb.AddForce(childRight * accel);
+	}
+}
