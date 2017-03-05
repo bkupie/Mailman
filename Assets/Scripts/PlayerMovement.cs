8d52ae3295d110db83c46fc85f6b896928405f40
lludko2 <lludko2@uic.edu>
2017-03-04 19:07:32 -0600

    initial push. has a simple closed gameworld and the ability to look around

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float rotationSpeed;
+    public float rotX;
+    public float rotY;
+    public float maxLookUpAngle;
+    public float maxLookDownAngle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotX += Input.GetAxis("Mouse X") * rotationSpeed;
+        rotY += -Input.GetAxis("Mouse Y") * rotationSpeed;
+        if (rotY > maxLookUpAngle) rotY = maxLookUpAngle;
+        if (rotY < maxLookDownAngle) rotY = maxLookDownAngle;
+
+        gameObject.transform.rotation = Quaternion.Euler(rotY, rotX, 0.0f);
+	}
+}
