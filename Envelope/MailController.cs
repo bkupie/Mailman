e14fc2420278675a9c9b4da326935c36a343b92e
bkupie <bkupie2@uic.edu>
2017-03-08 14:01:47 -0600

    Mail controller has the wait ability, and also spawns a few spaces ahead of user

--- Envelope/MailController.cs
+++ Envelope/MailController.cs
@@ -1,15 +1,15 @@
-sing System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MailController : MonoBehaviour
 {
-
+    // Use this for initialization
     public float speed = 10;
     public double height = 1;
+    public float lengthOfHold = 4.0f;
     public GameObject envelope;
     public double projectileSpeed = 1;
-    // Use this for initialization
 
     void Start()
     {
@@ -21,19 +21,37 @@ public class MailController : MonoBehaviour
     {
         float multipler = speed;
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            var mail = GameObject.Instantiate(envelope, this.transform.position, Quaternion.identity) as GameObject;
-            var objectCollider = this.GetComponentsInChildren<Collider>();
-            foreach (var collider in objectCollider)
-            {
-                Physics.IgnoreCollision(mail.GetComponent<Collider>(), collider);
-            }
+            StartCoroutine(ThrowMail());
+        
 
-            mail.GetComponent<Rigidbody>().velocity = transform.forward * -30 * (float)projectileSpeed;
-            //ball.GetComponent<Rigidbody>().velocity += Vector3.up * 10 * (float)height;
-            // so the mail drops with physics
-            this.GetComponent<Rigidbody>().AddForce(transform.forward * -800 * multipler);
 
-        }
     }
+    IEnumerator ThrowMail()
+    {
+
+        float multipler = speed;
+        var mail = GameObject.Instantiate(envelope, this.transform.position + transform.forward * 30 * Time.deltaTime, Quaternion.identity) as GameObject;
+        var objectCollider = this.GetComponentsInChildren<Collider>();
+        mail.GetComponent<Transform>().LookAt(this.transform.position);
+        // freeze mail so we can inspect it
+        mail.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+        // delay for 2 seconds so we can inspect the mail 
+
+        yield return new WaitForSeconds(lengthOfHold);
+        mail.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+        foreach (var collider in objectCollider)
+        {
+            Physics.IgnoreCollision(mail.GetComponent<Collider>(), collider);
+        }
+
+        mail.GetComponent<Rigidbody>().velocity = transform.forward * 30 * (float)projectileSpeed;
+        mail.GetComponent<Rigidbody>().velocity += Vector3.up * 5 * (float)height;
+
+
+
+    }
+
 }
+
+  
