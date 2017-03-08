661dd05a6b049adfdde99a8387f15488c29681fe
lludko2 <lludko2@uic.edu>
2017-03-07 23:37:09 -0600

    expanded worldspace. trees, more road, and some buildings were added, as requested. changes from our meeting also included

---
+++ Assets/Scripts/MailController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailController : MonoBehaviour
+{
+
+    public float speed = 10;
+    public double height = 1;
+    public GameObject envelope;
+    public double projectileSpeed = 1;
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float multipler = speed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var mail = GameObject.Instantiate(envelope, this.transform.position, Quaternion.identity) as GameObject;
+            var objectCollider = this.GetComponentsInChildren<Collider>();
+            foreach (var collider in objectCollider)
+            {
+                Physics.IgnoreCollision(mail.GetComponent<Collider>(), collider);
+            }
+
+            mail.GetComponent<Rigidbody>().velocity = transform.forward * 30 * (float)projectileSpeed;
+            mail.GetComponent<Rigidbody>().velocity += Vector3.up * 5 * (float)height;
+            // so the mail drops with physics
+         //   this.GetComponent<Rigidbody>().AddForce(transform.forward * -800 * multipler);
+
+        }
+    }
+}
