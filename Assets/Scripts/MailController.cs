f96b2823ca09d51467055ab4ee38ec5c7bca14c0
lludko2 <lludko2@uic.edu>
2017-03-08 23:38:46 -0600

    project integration of team contributions

--- Assets/Scripts/MailController.cs
+++ Assets/Scripts/MailController.cs
@@ -4,36 +4,47 @@ using UnityEngine;
 
 public class MailController : MonoBehaviour
 {
-
+    // Use this for initialization
     public float speed = 10;
     public double height = 1;
+    public float lengthOfHold = 4.0f;
     public GameObject envelope;
     public double projectileSpeed = 1;
-    // Use this for initialization
 
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float multipler = speed;
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            var mail = GameObject.Instantiate(envelope, this.transform.position, Quaternion.identity) as GameObject;
-            var objectCollider = this.GetComponentsInChildren<Collider>();
-            foreach (var collider in objectCollider)
-            {
-                Physics.IgnoreCollision(mail.GetComponent<Collider>(), collider);
-            }
+            StartCoroutine(ThrowMail());
 
-            mail.GetComponent<Rigidbody>().velocity = transform.forward * 30 * (float)projectileSpeed;
-            mail.GetComponent<Rigidbody>().velocity += Vector3.up * 5 * (float)height;
-            // so the mail drops with physics
-         //   this.GetComponent<Rigidbody>().AddForce(transform.forward * -800 * multipler);
 
+
+    }
+    IEnumerator ThrowMail() {
+
+        float multipler = speed;
+        var mail = GameObject.Instantiate(envelope, this.transform.position + transform.forward * 30 * Time.deltaTime, Quaternion.identity) as GameObject;
+        var objectCollider = this.GetComponentsInChildren<Collider>();
+        mail.GetComponent<Transform>().LookAt(this.transform.position);
+        // freeze mail so we can inspect it
+        mail.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+        // delay for 2 seconds so we can inspect the mail 
+
+        yield return new WaitForSeconds(lengthOfHold);
+        mail.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+        foreach (var collider in objectCollider) {
+            Physics.IgnoreCollision(mail.GetComponent<Collider>(), collider);
         }
+
+        mail.GetComponent<Rigidbody>().velocity = transform.forward * 30 * (float)projectileSpeed;
+        mail.GetComponent<Rigidbody>().velocity += Vector3.up * 5 * (float)height;
+
+
+
     }
 }
