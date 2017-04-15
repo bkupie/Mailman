b3edc9c44333f22c91159d1e393711e02a1e2f75
lludko2 <lludko2@uic.edu>
2017-04-15 03:39:01 -0500

    resolved some mouse stuff, sensitivity relaxed, colliders involved

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -6,25 +6,21 @@ using UnityStandardAssets.Characters.ThirdPerson;
 public class Footsteps : MonoBehaviour {
 
     AudioSource source;
-    AudioSource sprint;
 
 	// Use this for initialization
 	void Start () {
         source = gameObject.GetComponent<AudioSource>();
-	sprint = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //if () {
-        if (Input.GetKeyDown(KeyCode.W)) {
+        bool moveKeyDown = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S);
+        bool moveKeyUsed = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S);
+        if (moveKeyDown) {
             source.Play();
         }
-        else if (Input.GetKey(KeyCode.W) && !source.isPlaying) {
+        else if (moveKeyUsed && !source.isPlaying) {
             source.PlayDelayed(0.15f);
-        }
-	else if (Input.GetKeyDown(KeyCode.W)  && KeyCode.GetKeyDown(KeyCode.LeftShift) && !source.isPlaying && !sprint.isPlaying) {
-	    sprint.Play()		
         }
 	}
 }
--- Assets/Scripts/PlayerMovementMouse.cs
+++ Assets/Scripts/PlayerMovementMouse.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class PlayerMovementMouse : MonoBehaviour {
 
     public float rotationSpeed;
-    public float rotX;
-    public float rotY;
+    private float rotX = 0.0f;
+    private float rotY = 0.0f;
     public float maxLookUpAngle;
     public float maxLookDownAngle;
 
 	// Use this for initialization
 	void Start () {
-        
+        Vector3 startRotation = transform.localRotation.eulerAngles;
+        rotY = startRotation.y;
+        rotX = startRotation.x;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update() {
 
-        rotX += Input.GetAxis("Mouse X") * rotationSpeed;
+        rotY += Input.GetAxis("Mouse X") * rotationSpeed;
+        rotX -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        if (rotX > maxLookUpAngle) rotX = maxLookUpAngle;
+        if (rotX < maxLookDownAngle) rotX = maxLookDownAngle;
+
+        gameObject.transform.parent.rotation = Quaternion.Euler(0.0f, rotY, 0.0f);
+        //gameObject.transform.parent.rotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+        transform.rotation = Quaternion.Euler(rotX, rotY, 0.0f);
+
+        if (Input.GetKey(KeyCode.S)) {
+        }
+        
+        /*rotX += Input.GetAxis("Mouse X") * rotationSpeed;
         rotY += -Input.GetAxis("Mouse Y") * rotationSpeed;
         if (rotY > maxLookUpAngle) rotY = maxLookUpAngle;
         if (rotY < maxLookDownAngle) rotY = maxLookDownAngle;
 
         gameObject.transform.rotation = Quaternion.identity;
-        gameObject.transform.rotation = Quaternion.Euler(rotY, rotX, 0.0f);
+        gameObject.transform.rotation = Quaternion.Euler(rotY, rotX, 0.0f);*/
         //gameObject.transform.parent.transform.rotation = Quaternion.Euler(0, rotX, 0.0f);
             
 	}
