e046f84a8e2911034f386245fe3af6ca6509544a
bkupie <bkupie2@uic.edu>
2017-04-19 21:19:32 -0500

    Fixed jumping issue

--- Assets/Scripts/worldTimer.cs
+++ Assets/Scripts/worldTimer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class worldTimer : MonoBehaviour {
 
@@ -10,6 +11,7 @@ public class worldTimer : MonoBehaviour {
     private float time;
     Image image;
     public Canvas bigPanel;
+    private bool spacePressed;
 
     public UnityEngine.UI.Text scoreText;
     void Start () {
@@ -18,22 +20,27 @@ public class worldTimer : MonoBehaviour {
         timeLeft += 10;
         time = timeLeft;
         bigPanel.enabled = true;
+        spacePressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((time - timeLeft > 40 && bigPanel.enabled) || Input.GetKey(KeyCode.Space))
+        if((time - timeLeft > 40 && bigPanel.enabled) || Input.GetKey(KeyCode.Space) && spacePressed == false)
         {
             bigPanel.enabled = false;
             timeLeft = time;
+            spacePressed = true;
         }
         timeLeft -= Time.deltaTime;
         //scoreText.text = "Time left: " + timeLeft + " seconds.";
         image.fillAmount = timeLeft / time;
         scoreText.text = string.Format("{0:.#}",timeLeft);
-        if (timeLeft < 0 || (Input.GetKey(KeyCode.Escape))
-		)
+        if (timeLeft < 0 )
+        {
+            SceneManager.LoadScene(5);
+        }
+        if(Input.GetKey(KeyCode.Escape))
         {
             Application.Quit();
         }
