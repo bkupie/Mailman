4dd1cad1852b4fe80482fa6e2d811e2ed61c30a0
bkupie <bkupie2@uic.edu>
2017-04-19 20:32:15 -0500

    Scrolling text for game over

---
+++ Assets/Scripts/moveTextUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class moveTextUp : MonoBehaviour {
+    Vector3 temp;
+
+    public Text myText; 
+	// Use this for initialization
+	void Start () {
+		temp = new Vector3(0, 1.0f, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	        myText.transform.position += temp;
+
+    }
+}
